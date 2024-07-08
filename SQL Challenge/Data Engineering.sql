7b5a87bb2dee1688e4fa81e7355a48ef700bda75
Vvlaid <166451251+Vvlaid@users.noreply.github.com>
2024-07-08 12:18:56 -0400

    Updated

---
+++ SQL Challenge/Data Engineering.sql
@@ -0,0 +1,60 @@
+-- Had to create tables one by one while importing data for it to successfully import data
+
+
+-- Create all tables
+-- Create a table of titles
+CREATE TABLE Titles(
+	title_id VARCHAR(10) NOT NULL,
+	title VARCHAR(30) NOT NULL,
+	UNIQUE (title_id, title),
+	PRIMARY KEY (title_id)
+);
+
+-- Create a table of employees
+CREATE TABLE Employees(
+	emp_no INTEGER NOT NULL,
+	emp_title_id VARCHAR(10) NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES Titles(title_id),
+	birth_date DATE NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	gender VARCHAR(10) NOT NULL,
+	hire_date DATE NOT NULL,
+	UNIQUE (emp_no),
+	PRIMARY KEY (emp_no)
+);
+
+-- Create a table of departments
+CREATE TABLE Departments(
+	dept_no VARCHAR(10) NOT NULL,
+	dept_name VARCHAR(50) NOT NULL,
+	UNIQUE (dept_no),
+	PRIMARY KEY (dept_no)
+);
+
+-- Create a table of department employees
+CREATE TABLE Dept_Employees(
+	emp_no INTEGER NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+	dept_no VARCHAR(10) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
+);
+
+-- Create a table of department managers
+CREATE TABLE Dept_Managers(
+	dept_no VARCHAR(10) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES Departments(dept_no),
+	emp_no INTEGER NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+	PRIMARY KEY (dept_no, emp_no)
+);
+
+-- Create a table of salaries
+CREATE TABLE Salaries(
+	emp_no INTEGER NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES Employees(emp_no),
+	salary INTEGER NOT NULL,
+	UNIQUE (emp_no),
+	PRIMARY KEY (emp_no)
+);
---
+++ SQL Challenge/ERD/QuickDBD Export.sql
@@ -0,0 +1,76 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/xtT7Ci
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+
+CREATE TABLE "Employees" (
+    "emp_no" INTEGER   NOT NULL,
+    "emp_title_id" VARCHAR(10)   NOT NULL,
+    "birth_date" DATE   NOT NULL,
+    "first_name" VARCHAR(50)   NOT NULL,
+    "last_name" VARCHAR(50)   NOT NULL,
+    "sex" VARCHAR(10)   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Departments" (
+    "dept_no" VARCHAR(10)   NOT NULL,
+    "dept_name" VARCHAR(50)   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Titles" (
+    "title_id" VARCHAR(10)   NOT NULL,
+    "title" VARCHAR(30)   NOT NULL,
+    CONSTRAINT "pk_Titles" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "Dept_Employees" (
+    "emp_no" INTEGER   NOT NULL,
+    "dept_no" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_Dept_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Dept_Managers" (
+    "emp_no" INTEGER   NOT NULL,
+    "dept_no" VARCHAR(10)   NOT NULL,
+    CONSTRAINT "pk_Dept_Managers" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" INTEGER   NOT NULL,
+    "salary" INTEGER   NOT NULL,
+    CONSTRAINT "pk_Salaries" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "Titles" ("title_id");
+
+ALTER TABLE "Dept_Employees" ADD CONSTRAINT "fk_Dept_Employees_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_Employees" ADD CONSTRAINT "fk_Dept_Employees_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_Managers" ADD CONSTRAINT "fk_Dept_Managers_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_Managers" ADD CONSTRAINT "fk_Dept_Managers_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
